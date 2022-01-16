7cdfe13e1aef54b5de5cf8a2c1ad250a09b241ba
Diego Carrón Surribas <dcarron32@gmail.com>
2022-01-16 22:02:53 +0000

    Operadores de asignación

---
+++ ud05_t02_expresions_en_php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// Operadores aritméticos
+$x=10;
+$y=3;
+$z=$x*$y;
+echo $x." x ".$y." = ".$z."<br/>";
+$z=$x%$y;
+echo $x." módulo ".$y." = ".$z."<br/>";
+/*Operadores de asignación*/
+$z-=$y;
+echo "\$z-=\$y asigna a \$z".$z."<br/>";
+$z+=$y;
+echo "\$z+=\$x asigna a \$z".$z."<br/>";
+
+?>
+
+</body>
+</html>
