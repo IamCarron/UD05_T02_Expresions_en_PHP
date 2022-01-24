7bc17a86178536fb01945c7def4ef071cf110299
Diego Carrón Surribas <dcarron32@gmail.com>
2022-01-24 09:07:52 +0000

    Ejercicio 7

---
+++ ud05_t05_boletin_1/ejercicio7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nombre="Diego Carrón Surribas";
+    $direccion="Lalín";
+    $numero="777777777";
+    echo "$nombre <br/>";
+    echo "$direccion <br/>";
+    echo "$numero <br/>";
+    ?>
+</body>
+</html>
