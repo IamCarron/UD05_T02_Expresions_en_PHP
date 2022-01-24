b74ffbf002489016fc287f826c718aead7b28dcc
Diego Carrón Surribas <dcarron32@gmail.com>
2022-01-24 09:03:57 +0000

    Ejercicio 5

---
+++ ud05_t05_boletin_1/ejercicio5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x=(144);
+    $y=(999);
+
+    $suma=($x+$y);
+    $resta=($x-$y);
+    $division=($x/$y);
+    echo "La suma de $x y $y es: $suma<br/>";
+    echo "La resta de $x y $y es: $resta<br/>";
+    echo "La división de $x y $y es: $division";
+    ?>
+</body>
+</html>
