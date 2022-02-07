b2aee2ac99f66b7434f0c6dd15db6f394d7311bf
Diego Carrón Surribas <dcarron32@gmail.com>
2022-02-07 09:22:19 +0000

    Ejercicio2

---
+++ UD05_T10_Boletin_3/ejercicio2.php
@@ -0,0 +1,10 @@
+<?php
+    $resultado=explode(':',$_POST['N1']);
+    if ($resultado[0] <= '12') {
+        echo "Buenos días";
+    } elseif ($resultado[0] >= '13') {
+        echo "Buenas tardes";
+    } else {
+        echo "Buenas noches";
+    }
+    ?>
