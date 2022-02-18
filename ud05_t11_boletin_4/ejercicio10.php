9b79d93d14960914168fe1472a53541a1aec414e
Diego <dcarron32@gmail.com>
2022-02-18 08:51:02 +0000

    10

--- ud05_t11_boletin_4/ejercicio10.php
+++ ud05_t11_boletin_4/ejercicio10.php
@@ -11,14 +11,22 @@
 </head>
 <body>
     <?php
-    $_SESSION["numeros"]=array();
+
+    $numeros = array();
+    $_SESSION["numeros"]=$numeros;
     if(!isset($_REQUEST["N1"]) || $_REQUEST["N1"]>0){
+
+        if(isset($_REQUEST["N1"])){
+            array_push($_SESSION["numeros"],$_REQUEST["N1"]);
+        }
     ?>
     <form action="ejercicio10.php" method="post">
         Escriba un número: <input type="text" name="N1"/><br />
         <input type="submit" name="enviar" value="!Enviar¡">
     </form>
     <?php
+    }else{
+        /*calcular media */
     }
     ?>
 </body>
