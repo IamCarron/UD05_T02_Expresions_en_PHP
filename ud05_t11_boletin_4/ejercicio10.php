b1539268eb56d2f58aa1b48a09959d465109207b
Diego <dcarron32@gmail.com>
2022-02-21 08:46:27 +0000

    ejercicio10

--- ud05_t11_boletin_4/ejercicio10.php
+++ ud05_t11_boletin_4/ejercicio10.php
@@ -1,6 +1,5 @@
 <?php
-    session_start();
-?>
+session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,23 +10,39 @@
 </head>
 <body>
     <?php
-
-    $numeros = array();
-    $_SESSION["numeros"]=$numeros;
-    if(!isset($_REQUEST["N1"]) || $_REQUEST["N1"]>0){
-
-        if(isset($_REQUEST["N1"])){
-            array_push($_SESSION["numeros"],$_REQUEST["N1"]);
-        }
-    ?>
+    if (!isset($_SESSION["numeros"])) {
+        $_SESSION["numeros"] = [];
+    }
+    if (!isset($_REQUEST["N1"]) || $_REQUEST["N1"] > 0) {
+        if (isset($_REQUEST["N1"])) {
+            array_push($_SESSION["numeros"], $_REQUEST["N1"]);
+        } ?>
     <form action="ejercicio10.php" method="post">
         Escriba un número: <input type="text" name="N1"/><br />
         <input type="submit" name="enviar" value="!Enviar¡">
     </form>
     <?php
-    }else{
-        /*calcular media */
+    } else {
+        $sumarray = array_sum($_SESSION["numeros"]);
+        $countarray = count($_SESSION["numeros"]);
+        $calc = $sumarray / $countarray;
+        echo "La media es $calc";
+        $_SESSION = [];
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                "",
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+        session_destroy();
     }
+   
     ?>
 </body>
 </html>
