051b76386892a9adb226dcf2641bca9ba509a09e
Diego <dcarron32@gmail.com>
2022-02-21 09:32:35 +0000

    ejercicio11.1

---
+++ ud05_t11_boletin_4/ejercicio11.php
@@ -0,0 +1,54 @@
+<?php
+session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if (!isset($_SESSION["numeros"])) {
+        $_SESSION["numeros"] = [];
+    }
+    if (!isset($_REQUEST["N1"]) || $_REQUEST["N1"] > 1) || $COUNT < 5 {
+        if (isset($_REQUEST["N1"])) {
+            array_push($_SESSION["numeros"], $_REQUEST["N1"]);
+            $COUNT= count($_SESSION['N1']);
+        } ?>
+        <form action="ejercicio11.php" method="post">
+        Escriba un número: <input type="text" name="N1"/><br />
+        <input type="submit" name="enviar" value="!Enviar¡">
+    </form>
+    <?php
+    } else {
+        for ($i=0; $i <= 10; $i++) {
+            $solucion = $numero * $i;
+            echo "<tr>";
+            echo "<td>$numero</td>";
+            echo "<td>$i</td>";
+            echo "<td>$solucion</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        $_SESSION = [];
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                "",
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+        session_destroy();
+    }
+   
+    ?>
+</body>
+</html>
